cce2ecb5a2a16897ecd52b8271a94fbf3eb5e1e2
yashfa-iftikhar <iftikharyashfa888@gmail.com>
2025-06-17 22:47:42 +0500

    Update test_empty_email_password.py

--- tests/test_empty_email_password.py
+++ tests/test_empty_email_password.py
@@ -1,23 +1,21 @@
-# test_empty_fields.py
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
 import time
+from selenium import webdriver
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--disable-gpu")
+def create_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    return webdriver.Remote(command_executor='http://localhost:4444/wd/hub', options=options)
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+driver = create_driver()
 
 try:
-    driver.get("http://16.171.162.11:5100/login")
-    print("Opened login page with empty form")
+    driver.get("http://13.61.27.31:5100/login")
+    print("Opened login page")
 
-    # Click login without entering anything
     driver.find_element(By.XPATH, "//button[contains(text(), 'Login')]").click()
     time.sleep(2)
 
