82d853e2c64d3867f803320266edccbe6206754a
yashfa-iftikhar <iftikharyashfa888@gmail.com>
2025-06-29 14:44:05 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,44 +1,11 @@
 const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const authRoutes = require('./routes/authRoute');
-const categoryRoutes = require('./routes/categoryRoutes')
-const productRoutes = require('./routes/productRoute')
-const orderRoute = require('./routes/orderRoute')
-const cors = require('cors');
-const mongoose = require('mongoose');
 const app = express();
- 
-dotenv.config();
-
-const uri = process.env.MONGODB_URI;
-
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected!'))
-.catch(err => console.error(err));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: 'http://13.61.27.31:5100', // Adjust this to your frontend URL
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
-
-// Routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/category',categoryRoutes);
-app.use('/api/v1/product',productRoutes);
-app.use('/api/v1/order',orderRoute);
-
-
-//api
-app.get('/', (req, res) => {
-    res.send('Hello, World!');}
-)
 const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from ShopSphere!');
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
-
