166f0d4614f7c69bf033feadd8a0f5ed11cf68af
yashfa-iftikhar <iftikharyashfa888@gmail.com>
2025-06-29 14:08:25 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -38,4 +38,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');}
 )
 const PORT = process.env.PORT || 3000;
-app.listen(PORT)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
